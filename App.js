6362f3afa4980d28d8d4e7b2e15e05773dc6ba99
Pedro Pistone <aguspistone33@gmail.com>
2024-01-25 16:23:17 -0300

    miprimerpreentrega1

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Hola Coder!</Text>
+      <Text style={styles.introText}>
+        Sumérgete en un mundo de creatividad y expresión a través del arte digital. Descubre formas
+        únicas de representar ideas y emociones. Todo bien?
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2c3e50', // Cambié el color de fondo a un tono más oscuro
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  header: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#3498db', // Cambié el color del texto a un tono más claro
+    textAlign: 'center',
+  },
+  introText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#ecf0f1',
+    lineHeight: 24,
   },
 });
